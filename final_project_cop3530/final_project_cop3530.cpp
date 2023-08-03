6c487b55fc3f7d92105027c99cdbc40ce30a25b5
Gbigott <92874470+Gbigott@users.noreply.github.com>
2023-08-03 01:15:03 -0400

    Update final_project_cop3530.cpp
    creation of structure, update of readfile and creation of heap function (temporal, will be updated).

--- final_project_cop3530/final_project_cop3530.cpp
+++ final_project_cop3530/final_project_cop3530.cpp
@@ -3,30 +3,56 @@
 #include<fstream>
 #include<vector>
 #include<sstream>
+#include <algorithm>  
 #include<map>
 
 using namespace std;
 
 struct RecordsOfHomes
 {
-    RecordsOfHomes (int rooms_beds,int prices,string city)
+    RecordsOfHomes ( double ids,int rooms_beds,int prices,string city)
     {
+        IDS = ids;
         Room = rooms_beds;
+        Prices = prices;
+        City = city;
+
 
     }
 
     int Room;
     int Prices;
+    double IDS;
     string City;
 
   
 };
 
-int main()
+bool operator <(const RecordsOfHomes& i, const RecordsOfHomes& j)
+{
+    return i.IDS < j.IDS;
+}
+
+void printVec(vector<RecordsOfHomes> row) {
+    
+    for (int i = 0; i < 10; i++) {
+        cout << row[i].IDS <<" "<<row[i].City<<" "<<row[i].Prices<<" "<<row[i].Room<<endl;
+    }
+}
+vector<RecordsOfHomes> insert_on_heap(vector<RecordsOfHomes>& info)
+{
+  vector<RecordsOfHomes> temp = info;
+ make_heap (info.begin(),info.end());
+
+ return temp;
+
+}
+
+vector<RecordsOfHomes> Read_file(string in_file)
 {
     ifstream infile;
-    vector<RecordsOfHomes> info;
-    infile.open("housing.csv");
+    vector<RecordsOfHomes> saved_info;
+    infile.open(in_file);
     
     if(!(infile.is_open()))
     {
@@ -35,12 +61,12 @@ int main()
     else
     {
         string s;
-        string temp;
+        
         cout << " processing info..." << endl;
+        getline(infile, s);
 
         while(getline(infile,s)){
-            //temp = s.substr(1);
-            string ids;
+            double ids;
             string url;
             string region;
             string region_url;
@@ -66,7 +92,8 @@ int main()
 
             stringstream inputString(s);
 
-            getline(inputString,ids,',');
+            getline(inputString,temp,',');
+            ids = atof(temp.c_str());
             getline(inputString,url,',');
             getline(inputString,region,',');
             getline(inputString,region_url,',');
@@ -90,31 +117,86 @@ int main()
             getline(inputString,long_s,',');
             getline(inputString,state,',');
 
-            RecordsOfHomes records(beds, price, region);
+            RecordsOfHomes records(ids,beds, price, region);
 
-            info.push_back(records);
+            saved_info.push_back(records);
 
              
              
         }
         infile.close();
 
+
+
         
     }
+
+    return saved_info;
+
+}
+int main()
+{
+    
+    vector<RecordsOfHomes> info;
+    string file = "housing.csv";
+    info = Read_file("housing.csv");
     
     
     cout<<"Welcome to our housing search app! please choose your search method:"<<endl;
     cout<<"1.search by room count"<<endl;
     cout<<"2.search by area"<<endl;
-
+    
     int selectOption;
-
     cin>>selectOption;
 
     if(selectOption == 1)
     {
-        cout<<"please select the minimum number of rooms to search for:"<<endl;
+        int selection_op;
+        cout<<"1. Use maps"<<endl;
+        cout<<"2.Use maxheap"<<endl;
+        cin>>selection_op;
 
+        if(selection_op == 1)
+        {
+            //insert your map function here.
+            cout<<"please select the minimum number of rooms to search for:"<<endl;
+        }
+        else if(selection_op == 2)
+        {
+            vector<RecordsOfHomes> insertheap = insert_on_heap(info);
+
+            printVec(insertheap);
+
+
+            cout<<"please select the minimum number of rooms to search for:"<<endl;
+
+        }
+        
+
+       
+
+    }
+    else if(selectOption == 2)
+    {
+        int selection_op;
+        cout<<"1. Use maps"<<endl;
+        cout<<"2.Use minheap"<<endl;
+        cin>>selection_op;
+
+        if(selection_op == 1)
+        {
+            //insert your map function here.
+            cout<<"please enter the name of the city you looking for:"<<endl;
+            string city_name;
+            cin >> city_name;
+
+        }
+        else if(selection_op == 2)
+        {
+            cout<<"please select the minimum number of rooms to search for:"<<endl;
+             string city_name;
+             cin >> city_name;
+        }
     }
 
 
